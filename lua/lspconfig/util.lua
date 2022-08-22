982c58d650a5d70ffaa065baa65ad2897f9aef53
William Boman <william@redwill.se>
2022-08-22 21:13:20 +0200

    fix: only provide unpacked fargs to command functions

--- lua/lspconfig/util.lua
+++ lua/lspconfig/util.lua
@@ -87,8 +87,8 @@ end
 function M.create_module_commands(module_name, commands)
   for command_name, def in pairs(commands) do
     local opts = M._parse_user_command_options(def)
-    api.nvim_create_user_command(command_name, function(...)
-      require('lspconfig')[module_name].commands[command_name][1](...)
+    api.nvim_create_user_command(command_name, function(info)
+      require('lspconfig')[module_name].commands[command_name][1](unpack(info.fargs))
     end, opts)
   end
 end
